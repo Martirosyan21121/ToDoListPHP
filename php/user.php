ea46bd6a1ecb49d85b79ceee760e753e640213f8
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-04-23 14:59:23 +0400

    login register

---
+++ php/user.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'db_connection.php';
+
+class User extends db_connection
+{
+    public function register($username, $email, $password)
+    {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+        return $this->connection->query($sql);
+    }
+
+    public function login($email, $password)
+    {
+        $sql = "SELECT * FROM todo.user WHERE email='$email'";
+        $result = $this->connection->query($sql);
+
+        if ($result->num_rows == 1) {
+            $user = $result->fetch_assoc();
+            if (password_verify($password, $user['password'])) {
+                return $user;
+            }
+        }
+        return false;
+    }
+}
