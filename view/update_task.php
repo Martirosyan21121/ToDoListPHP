1b8a95ce3fdd7d01e0e7e98f7ca74ceff309f25f
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-05-02 09:23:30 +0400

    login register

--- view/update_task.php
+++ view/update_task.php
@@ -6,7 +6,7 @@ session_start()
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Update page</title>
+    <title>Update Task</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <script type="application/x-javascript"> addEventListener("load", function () {
